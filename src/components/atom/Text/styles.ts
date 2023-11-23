c4ce93597c0f0b39188be3c5cd2fba0c17eb0775
Cleydson Souza <c_cesouza@solarbr.com.br>
2023-11-23 06:59:47 -0300

    feat: create a text component atom

--- src/components/atom/Text/styles.ts
+++ src/components/atom/Text/styles.ts
@@ -1,12 +1,23 @@
 import styled from 'styled-components/native';
+import themes from "../../../themes";
 
 export interface ITextProps {
   text?: string;
   fontSize?: number;
-  color?: string;
+  color?: string | 'white' | 'dark';
+  type?: 'bold' | 'medium' | 'small';
+  align?: string | 'center' | 'left';
 }
 
 export const Container = styled.Text<ITextProps>`
-  color: ${props => props.color || '#000'};
+  color: ${props =>
+    props.color || props.color === 'white'
+      ? themes.colors.white
+      : props.color === 'dark'
+      ? themes?.colors?.dark
+      : '#000'};
   font-size: ${props => props.fontSize || 12};
+  font-weight: ${props =>
+    props.type === 'bold' ? 700 : props.type === 'medium' ? 500 : 400};
+  text-align: ${props => (props.align === 'center' ? 'center' : 'left')};
 `;
