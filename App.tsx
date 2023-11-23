d296f667dcb24429f923ddf1e106ccb4bace722b
Cleydson Souza <c_cesouza@solarbr.com.br>
2023-11-23 07:02:18 -0300

    feat: using gesture-handler

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { Welcome } from './src/components/template/Welcome';
 import themes from './src/themes';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={themes}>
-      <Welcome  />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <ThemeProvider theme={themes}>
+        <Welcome />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 };
